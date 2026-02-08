a190038751424d362784d83a1f06da3cfbf2c2ef
ajitaabhardwaj <44480136+ajitaabhardwaj@users.noreply.github.com>
2026-02-08 15:23:56 -0700

    Time: 0 ms (100.00%) | Memory: 10.7 MB (21.45%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,32 +1,38 @@
 class Solution {
 public:
+
+    int solve(int i, vector<int>& nums, vector<int>& sums)
+    {
+        if(i == nums.size()-1 || i == nums.size()-2)
+        {
+            sums[i] = nums[i];
+            return sums[i];
+        }
+        if(i>=nums.size())
+        {
+            return 0;
+        }
+        if(sums[i] != -1)
+        {
+            return sums[i];
+        }
+        sums[i] = max(solve(i+2, nums, sums) + nums[i], solve(i+3, nums, sums) + nums[i]);
+        return sums[i];
+    }
+
     int rob(vector<int>& nums) 
     {
-        int n= nums.size();
-
-        vector<int> sums (n, 0);
+        vector<int> sums (nums.size(), -1);
         int ans = 0;
+        if(nums.size() ==2)
+        {
+            return max(nums[0], nums[1]);
+        }
 
-        if(nums.size() == 1)
+        if(nums.size() <=1)
         {
             return nums[0];
         }
-        sums[n-1] = nums[n-1];
-        sums[n-2] = nums[n-2];
-
-        ans = max (sums[n-1], sums[n-2]);
-
-        for(int i=n-3; i>=0; i--)
-        {
-            if(i+3 < n)
-            {
-                sums[i] = max ((sums[i+2] + nums[i]), (sums[i+3] + nums[i]));
-            }
-            else{
-                sums[i] = nums[i] + sums[i+2];
-            }
-            ans = max(ans, sums[i]);
-        }
-        return ans;
+        return max(solve(0, nums, sums), solve(1, nums, sums));
     }
 };
