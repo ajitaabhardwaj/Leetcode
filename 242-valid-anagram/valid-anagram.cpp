0338ac45ceb140eeae2427cbcea22fdfb1238dec
ajitaabhardwaj <44480136+ajitaabhardwaj@users.noreply.github.com>
2026-02-09 20:08:40 -0700

    Time: 10 ms (7.40%) | Memory: 9.9 MB (10.72%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -2,22 +2,37 @@ class Solution {
 public:
     bool isAnagram(string s, string t) 
     {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-       
-        int i=0;
-
         if(s.length() != t.length())
             return false;
+        
+        unordered_map<char, int> mp1;
+        unordered_map<char, int> mp2;
 
-        while(i<s.length() && i<t.length())
+        int i=0;
+        while(i<s.length())
         {
-            if(s[i] != t[i])
+
+            if(mp1.find(s[i]) == mp1.end())
             {
-                return false;
+                mp1[s[i]] = 1;
+            }
+            else{
+                mp1[s[i]]++;
+            }
+            if(mp2.find(t[i]) == mp2.end())
+            {
+                mp2[t[i]] = 1;
+            }
+            else{
+                mp2[t[i]]++;
             }
             i++;
         }
-        return true;
+
+        if(mp1 == mp2)
+        {
+            return true;
+        }
+        return false;
     }
 };
