e3063f0f14a582b619dbc7b18e45e9a218d7ac02
ajitaabhardwaj <44480136+ajitaabhardwaj@users.noreply.github.com>
2026-02-12 11:59:23 -0700

    Time: 3 ms (37.77%) | Memory: 42.2 MB (16.37%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -2,17 +2,35 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) 
     {
+        vector<int> fr(nums.size(), 1);
+        vector<int> bck(nums.size(), 1);
         int numofzero = 0;
+        fr[0] = 1;
+        bck[nums.size()-1] = 1;
         int prod = 1;
-        for(int i=0; i<nums.size(); i++)
+        int prod2 = 1;
+
+        for(int i=0 ; i<nums.size(); i++)
         {
             if(nums[i] == 0)
             {
                 numofzero++;
+                fr[i] = 1;
+                continue;
             }
-            else{
-                prod = prod * nums[i];
+            fr[i] = prod;
+            prod = prod * nums[i];  
+        }
+
+        for(int i=nums.size()-1; i>=0; i--)
+        {
+            if(nums[i] == 0)
+            {
+                bck[i] = nums[i];
+                continue;
             }
+            bck[i] = prod2;
+            prod2 = prod2 * nums[i];  
         }
 
         if(numofzero > 1)
@@ -23,7 +41,7 @@ public:
         else if(numofzero == 0){
             for(int i=0; i<nums.size(); i++)
             {
-                nums[i] = prod / (nums[i]);
+                nums[i] = fr[i] * bck[i];
             }
         }
         else{
