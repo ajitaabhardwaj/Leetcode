c69ba590032c57466c090de58123d0156567bcb6
ajitaabhardwaj <44480136+ajitaabhardwaj@users.noreply.github.com>
2026-05-28 17:31:26 -0700

    Time: 7 ms (30.84%) | Memory: 19.3 MB (21.61%) - LeetSync

---
+++ 3606-minimum-element-after-replacement-with-digit-sum/minimum-element-after-replacement-with-digit-sum.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minElement(self, nums: List[int]) -> int:
+        ans = 1000000
+        for num in nums:
+            digsum = 0
+
+            while num>0:
+                n = num%10
+                num = num//10
+                digsum += n
+            ans = min(ans, digsum)
+
+        return ans
+            
+
+
+
+        
