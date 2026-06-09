327f9646bd565c5b7852931c6c58a925caa04d00
ajitaabhardwaj <44480136+ajitaabhardwaj@users.noreply.github.com>
2026-06-08 20:30:33 -0700

    Time: 23 ms (7.28%) | Memory: 21.1 MB (52.43%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,14 +1,11 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        count = 1
-        i = 1
-        nums.sort()
-        while i<len(nums):
-            if nums[i] == nums[i-1]:
-                count+=1
+        mp = {}
+        for i in nums:
+            if i not in mp:
+                mp[i] = 1
             else:
-                count=1
-            if count > len(nums)/2:
-                return nums[i]
-            i+=1
+                mp[i]+=1
+            if mp[i] > len(nums)/2:
+                return i
         return nums[0]
