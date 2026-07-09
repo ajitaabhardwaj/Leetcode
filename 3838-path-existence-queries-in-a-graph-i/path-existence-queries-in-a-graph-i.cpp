0773331ce1123a1e3f4ce4f96343248d29665bb8
ajitaabhardwaj <44480136+ajitaabhardwaj@users.noreply.github.com>
2026-07-08 20:06:03 -0700

    Time: 4 ms (93.70%) | Memory: 224.1 MB (66.03%) - LeetSync

--- 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.cpp
+++ 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.cpp
@@ -2,53 +2,28 @@ class Solution {
 public:
     vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
         vector<bool> ans;
-        unordered_map<int, int> mp;
+        vector<int> mp(n);
+        int x = 0;
         mp[0] = 0;
-        int a = 0;
             for(int i=1; i<n; i++)
             {
-                if(nums[i]-nums[i-1] <= maxDiff)
+                if(nums[i]-nums[i-1] > maxDiff)
                 {
-                    mp[a] = i;
+                    x++;
                 }
-                else{
-                    a = i;
-                }
+                mp[i] = x;
             }
-
+            
         for(int q=0; q<queries.size(); q++)
         {
             int src = queries[q][0];
             int dest = queries[q][1];
-            if(src == dest)
+            
+            if(mp[src] == mp[dest])
             {
                 ans.push_back(true);
-                continue;
-            }
-            int s;
-            int d = dest;
-            if(src<=dest)
-            {
-                s = src;
             }
             else{
-                s = dest;
-                d = src;
-            }
-
-            int flag =0;
-            for(auto& [key, value] : mp)
-            {
-                if(s >= key && d <= value)
-                {
-                    flag =1;
-                    ans.push_back(true);
-                    break;
-                }
-            }
-
-            if(flag == 0)
-            {
                 ans.push_back(false);
             }
         }
