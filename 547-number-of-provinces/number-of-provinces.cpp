65fb8d00f9c7c09072cacce3ecca6d42b943b9bc
ajitaabhardwaj <44480136+ajitaabhardwaj@users.noreply.github.com>
2026-08-06 18:12:27 -0700

    Time: 0 ms (100.00%) | Memory: 19.5 MB (71.79%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,39 +1,41 @@
 class Solution {
 public:
-    void dfs(int n, vector<bool>& visited, vector<vector<int>>& isConnected)
-    {
-        for(int i=0; i<visited.size(); i++)
-        {
-            if(n==i)
-            {
-                continue;
-            }
-            if(isConnected[n][i] == 1 && visited[i] == false)
-            {
-                visited[i] = true;
-                dfs(i, visited, isConnected);
-            }
-        }
-        cout<<endl;
+
+    int find(int node, vector<int>& parent){
+        if(node == parent[node])
+            return node;
+        return parent[node] = find(parent[node], parent);
+    }
+
+    void unions(int a, int b, vector<int>& parent){
+        int rootA = find(a, parent);
+        int rootB = find(b, parent);
+            parent[rootB] = rootA;
         return;
     }
-    int findCircleNum(vector<vector<int>>& isConnected) 
-    {
-        int ans=0;
-        int n = isConnected.size();
 
-        vector<bool> visited(n, false);
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        if(isConnected.size() ==1)
+            return 1;
 
-        for(int i=0; i<n; i++)
-        {
-            if( visited[i] == false)
-            {
-                visited[i] == true;
-                dfs(i, visited, isConnected);
-                ans++;
+        vector<int> parent(isConnected.size(), 0);
+        for(int i=0; i<isConnected.size(); i++)
+            parent[i] = i;
+
+        for(int i=0; i<isConnected.size(); i++){
+            for(int j=0; j<isConnected.size(); j++){
+                if(i==j)
+                    continue;
+                if(isConnected[i][j] == 1){
+                    unions(i, j, parent);
+                }
             }
         }
+        int ans = 0;
+        for(int i=0; i<parent.size(); i++){
+            if (parent[i] == i)
+                ans++;
+        }
         return ans;
-        
     }
 };
