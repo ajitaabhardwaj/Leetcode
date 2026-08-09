c3213ce424b2ad35b6222d7c0db8df95458a6aeb
ajitaabhardwaj <44480136+ajitaabhardwaj@users.noreply.github.com>
2026-08-09 11:17:15 -0700

    Time: 92 ms (8.37%) | Memory: 45.3 MB (16.18%) - LeetSync

---
+++ 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int nearestExit(vector<vector<char>>& maze, vector<int>& entrance) {
+        int m = maze.size();
+        int n = maze[0].size();
+        vector<vector<int>> steps { {-1, 0}, {0, -1}, {1,0}, {0,1} };
+        vector<vector<bool>> visited(m, vector<bool>(n, false));
+        queue<vector<int>> q;
+        int ans=0;
+
+        q.push(entrance);
+        visited[entrance[0]][entrance[1]] = true;
+        q.push({-1, -1});
+        while(!q.empty()){
+            int i = q.front()[0];
+            int j = q.front()[1];
+            q.pop();
+
+            if(i == -1 and j == -1){
+                ans++;
+                cout<<ans<<endl;
+                if(!q.empty()) q.push({-1, -1});
+                continue;
+            }
+
+            
+            for(auto& step: steps){
+                int nbi = i + step[0];
+                int nbj = j + step[1];
+
+                if(nbi<0 or nbi>=m or nbj<0 or nbj>=n){
+                    continue;
+                }
+                
+                if(maze[nbi][nbj] == '.' and visited[nbi][nbj] == false){
+
+                    if(nbi == 0 or nbj == 0 or nbi == m-1 or nbj == n-1 ){
+                    return ++ans;
+                    }
+                    visited[nbi][nbj] = true;
+                    q.push({nbi, nbj});
+                }
+            }
+            
+        }
+        return -1;
+        
+    }
+};
