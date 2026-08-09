042f549c8284a431d1634657c4974fe279814339
ajitaabhardwaj <44480136+ajitaabhardwaj@users.noreply.github.com>
2026-08-09 12:23:40 -0700

    Time: 4 ms (69.57%) | Memory: 98.9 MB (82.38%) - LeetSync

--- 1661-minimum-number-of-vertices-to-reach-all-nodes/minimum-number-of-vertices-to-reach-all-nodes.cpp
+++ 1661-minimum-number-of-vertices-to-reach-all-nodes/minimum-number-of-vertices-to-reach-all-nodes.cpp
@@ -4,10 +4,6 @@ public:
         unordered_map<int, vector<int>> adj;
         vector<int> indegree(n, 0);
         for(int i=0; i<edges.size(); i++){
-            adj[edges[i][0]].push_back(edges[i][1]);
-            if(adj.find(edges[i][1]) == adj.end()){
-                adj[edges[i][1]] = {};
-            }
             indegree[edges[i][1]]++;
         }
         vector<int> ans;
