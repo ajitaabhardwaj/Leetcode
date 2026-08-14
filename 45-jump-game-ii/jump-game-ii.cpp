6ee42280d76cbc987b7bceee3a718a7be6eb6c54
ajitaabhardwaj <44480136+ajitaabhardwaj@users.noreply.github.com>
2026-08-13 18:30:22 -0700

    Time: 0 ms (100.00%) | Memory: 20.7 MB (29.46%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
-    int dp(int i, vector<int>& nums, vector<int>& memo){
-        if(i>=nums.size()-1) return 0;
-        if(memo[i] != -1) return memo[i];
-        
-        int jump = nums[i];
-        int mini = INT_MAX;
-        for(int j=1; j<=jump; j++){
-            int nextjump = dp(i+j, nums, memo);
-            if(nextjump != INT_MAX) mini = min(mini, 1+nextjump);
-        }
-        return memo[i] = mini;
-    }
     int jump(vector<int>& nums) {
-        vector<int> memo(nums.size(), -1);
+        int l = 0;
+        int r = 0;
+        int count=0;
+        while(r<nums.size()-1){
+            int far = 0;
+            for(int i=l; i<=r; i++){
+                far = max(far, i+nums[i]);
+            }
+            count++;
+            l+=1;
+            r=far;
+            if(far>=nums.size()-1) break;
 
-        return dp(0, nums, memo);
-
-
+        }
+        return count;
     }
 };
