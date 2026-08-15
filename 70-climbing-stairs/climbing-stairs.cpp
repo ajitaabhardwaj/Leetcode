30855e5358866bca10802b02197d0bed14150b1e
ajitaabhardwaj <44480136+ajitaabhardwaj@users.noreply.github.com>
2026-08-15 09:42:05 -0700

    Time: 2 ms (5.04%) | Memory: 8.7 MB (20.93%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,24 +1,14 @@
 class Solution {
 public:
-    int sol(int n, vector<int>& memo){
-        if(n<0){
-            return 0;
-        }
-        if(n==0)
-        {
-            return 1;
-        }
-        if(memo[n]!=-1){
-            return memo[n];
-        }
-        memo[n] = sol(n-1, memo) + sol(n-2, memo);
-        return memo[n];
-    }
-
+    
     int climbStairs(int n) {
-        vector<int> memo(n+1, -1);
-
-        memo[0]= 0;
-        return sol(n, memo);
+        if(n<=2) return n;
+        vector<int> memo(n+1, 0);
+        memo[1] = 1;
+        memo[2] = 2;
+        for(int i=3; i<=n ; i++){
+            memo[i] = memo[i-1] + memo[i-2];
+        }
+        return memo[n];
     }
 };
