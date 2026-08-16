13c2ea14a07a0c5b5bbd0e71a4d69cb55eaa8941
ajitaabhardwaj <44480136+ajitaabhardwaj@users.noreply.github.com>
2026-08-15 19:50:37 -0700

    Time: 111 ms (68.50%) | Memory: 111.1 MB (20.75%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,20 +1,6 @@
 class Solution {
 public:
-    int dp(int i,int target, vector<int>& nums, vector<vector<int>>& memo){
-        if(target<0) return 0;
-        if(i==nums.size()){
-            if(target == 0){
-                return 1;
-            }
-            else{
-                return 0;
-            }
-        }
-        
-        if(memo[i][target] != -1) return memo[i][target];
-
-        return memo[i][target] = dp(i+1, target-nums[i], nums, memo) or dp(i+1, target, nums, memo);        
-    }
+    
     bool canPartition(vector<int>& nums) {
         int total = 0;
         for(int i=0; i<nums.size(); i++){
@@ -22,9 +8,16 @@ public:
         }
         if(total%2 ==1) return false;
         int target = total/2;
-        vector<vector<int>> memo(nums.size(), vector<int>(target+1, -1));
-        int ans = dp(0, target, nums, memo);
-        if(ans==0) return false;
-        else return true;
+        vector<vector<int>> memo(nums.size()+1, vector<int>(target+1, 0));
+        memo[nums.size()][0] = 1;
+        for(int i=nums.size()-1; i>=0; i--){
+            for(int j=target; j>0; j--){
+                if(nums[i]>j) memo[i][j] = memo[i+1][j];
+                else{
+                    memo[i][j] = memo[i+1][j-nums[i]] || memo[i+1][j];
+                }
+            }
+        }
+        return memo[0][target];
     }
 };
