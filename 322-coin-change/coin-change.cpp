fdec582bc8fd905f0082f9d1d31aeb48867fa2f9
ajitaabhardwaj <44480136+ajitaabhardwaj@users.noreply.github.com>
2026-08-15 20:48:29 -0700

    Time: 47 ms (35.32%) | Memory: 53.1 MB (11.33%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,34 +1,24 @@
-#include <climits>
 class Solution {
 public:
-
-    long long int sol(int i, int remamount, vector<int>& coins, vector<vector<int>>& memo)
-    {
-        if(i==coins.size() || remamount<0 ){
-            return 1e9;
-        }
-
-        if(memo[i][remamount] != -1){
-            return memo[i][remamount];
-        }
-
-        if(remamount == 0){
-            return 0;
+    int coinChange(vector<int>& coins, int amount){
+        vector<vector<int>> dp(coins.size()+1, vector<int>(amount+1, 0) );
+        
+        dp[coins.size()][0] = 0;
+        for(int j=1; j<=amount; j++){
+            dp[coins.size()][j] = 1e9;
         }
         
-        memo[i][remamount] =  min( 
-            sol(i+1, remamount, coins, memo), 
-            1+ sol(i, remamount-coins[i], coins, memo)
-            );
+        for(int i=coins.size()-1; i>=0; i--){
+            for(int j=0; j<=amount; j++){
         
-        return memo[i][remamount];
-    }
-    
-    int coinChange(vector<int>& coins, int amount) {
-        vector<vector<int>> memo(coins.size()+1, vector<int>(amount+1, -1));
-
-        long long int ans = sol(0, amount, coins, memo);
-        return (ans < 1e9) ? ans: -1; 
-     
+                if (j-coins[i] < 0) dp[i][j] = dp[i+1][j];
+                else dp[i][j] = min ( 1+dp[i][j-coins[i]] , dp[i+1][j] );
+            }
+        }
+        if (dp[0][amount]>=1e9) 
+        {
+            return -1;
+        }
+        else return dp[0][amount];
     }
 };
