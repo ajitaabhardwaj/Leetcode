89a698008f9b963b567240eba77fbbfe7a99a7aa
ajitaabhardwaj <44480136+ajitaabhardwaj@users.noreply.github.com>
2026-08-15 19:33:34 -0700

    Time: 63 ms (88.72%) | Memory: 110.5 MB (33.83%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,51 +1,30 @@
 class Solution {
 public:
-
-    int memo(int i, int total, vector<int>& nums,  vector<vector<int>>& dp){
-        if(total == 0){
-            return 1;
-        }
+    int dp(int i,int target, vector<int>& nums, vector<vector<int>>& memo){
+        if(target<0) return 0;
         if(i==nums.size()){
-            return 0;
+            if(target == 0){
+                return 1;
+            }
+            else{
+                return 0;
+            }
         }
-        if(total<0){
-            return 0;
-        }
-
-        if(dp[i][total] != -1)
-        {
-            return dp[i][total];
-        }
-
         
-        dp[i][total] = ( memo(i+1, total, nums, dp) || memo(i+1, total-nums[i], nums, dp) );
-        return  dp[i][total];
-        
+        if(memo[i][target] != -1) return memo[i][target];
+
+        return memo[i][target] = dp(i+1, target-nums[i], nums, memo) or dp(i+1, target, nums, memo);        
     }
-
-
-    bool canPartition(vector<int>& nums) 
-    {
+    bool canPartition(vector<int>& nums) {
         int total = 0;
-
-        for(int i=0; i<nums.size(); i++)
-        {
+        for(int i=0; i<nums.size(); i++){
             total+=nums[i];
         }
-        if(total%2 !=0)
-        {
-            return false;
-        }
-        total = total/2;
-
-        vector<vector<int>> dp(nums.size()+1, vector<int> (total+1, -1));
-
-        if (memo(0, total, nums, dp) == 0)
-        {
-            return false;
-        }
-        else{
-            return true;
-        }
+        if(total%2 ==1) return false;
+        int target = total/2;
+        vector<vector<int>> memo(nums.size(), vector<int>(target+1, -1));
+        int ans = dp(0, target, nums, memo);
+        if(ans==0) return false;
+        else return true;
     }
 };
