64e3d9d637e756218d39b32e81fe50e8106a8be2
ajitaabhardwaj <44480136+ajitaabhardwaj@users.noreply.github.com>
2026-08-16 01:46:41 -0700

    Time: 255 ms (36.20%) | Memory: 296.6 MB (13.66%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
-    //i
-    // take - make it as prev (this > prev)
-    // dont take
-    int dp(int i, int prev, vector<int>& nums, vector<vector<int>>& memo){
-        if(i>=nums.size()) 
-            return 0;
 
-        if(memo[i][prev+1] != -1) 
-            return memo[i][prev+1];
-
-        int op1 = dp(i+1, prev, nums, memo);
-        int op2 = 0;
-        if (prev==-1 or nums[i]>nums[prev])
-            op2 = 1 + dp(i+1, i, nums, memo);
-
-        return memo[i][prev+1] = max(op1, op2);
-    }
     int lengthOfLIS(vector<int>& nums) {
-        vector<vector<int>> memo(nums.size()+1, vector<int>(nums.size()+1, -1));
-        int ans = dp(0, -1, nums, memo);
-        return ans;
+        int n=nums.size();
+        vector<vector<int>> memo(n+1, vector<int>(n+1, 0));
+        for(int i=0; i<=n; i++) memo[n][i] =0;
+        for(int i=n-1; i>=0; i--){
+            for(int prev = i-1; prev>=-1; prev--){
+                if(prev == -1 or nums[i]>nums[prev]){
+                    memo[i][prev+1] = max( 1+ memo[i+1][i+1], memo[i+1][prev+1]);
+                }
+                else{
+                     memo[i][prev+1] = memo[i+1][prev+1];
+                }
+            }
+        }
+        return memo[0][0];
+
+
     }
 };
