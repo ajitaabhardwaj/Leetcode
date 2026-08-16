fea6de1e84a54c25fc8cb5a07c7e3eba9be08c00
ajitaabhardwaj <44480136+ajitaabhardwaj@users.noreply.github.com>
2026-08-16 02:51:19 -0700

    Time: 8 ms (44.95%) | Memory: 13.2 MB (61.30%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,44 +1,22 @@
-#include <cmath>
-
 class Solution {
 public:
-
-    int lcs(int i, int j, string& word1, string& word2, vector<vector<int>>& memo){
-
-        if(i==word1.length() ){
-            return word2.length()-j;
-        }
-
-        if(j== word2.length()){
-            return word1.length()-i;
-        }
-
-        if(memo[i][j] != -1)
-        {
-            return memo[i][j];
-        }
-
-        if(word1[i] == word2[j]){
-            memo[i][j] = lcs(i+1, j+1, word1, word2, memo); 
-        }
-
-        if(word1[i] != word2[j]){
-            memo[i][j] = min(
-              { 1+ lcs(i+1, j, word1, word2, memo), 
-                1+ lcs(i, j+1, word1, word2, memo),
-                1 + lcs(i+1, j+1, word1, word2, memo) }
-            );
-        }
-        return memo[i][j];
-    }
-
     int minDistance(string word1, string word2) {
-       
-       vector<vector<int>> memo (word1.length()+1, vector<int> (word2.length()+1, -1));
+        int m = word1.length();
+        int n = word2.length();
+        vector<vector<int>> memo(m+1, vector<int>(n+1, 0));
+        for(int j = 0; j<=n; j++) memo[m][j] = word2.length()-j;
+        for(int i = 0; i<=m; i++) memo[i][n] = word1.length()-i;
 
-        int ans = lcs(0, 0, word1, word2, memo);
-
-       
-        return ans;
+        for(int i=m-1; i>=0; i--){
+            for(int j=n-1; j>=0; j--){
+                if(word1[i] == word2[j]){
+                    memo[i][j] = memo[i+1][j+1];
+                }
+                else{
+                    memo[i][j] = 1+ min({ memo[i+1][j+1], memo[i+1][j], memo[i][j+1] });
+                }
+            }
+        }
+        return memo[0][0];
     }
 };
